44ba7919faf88b25be379ec015693d314bc72bfa
DmitryAlCh <dmi.al.ch@gmail.com>
2017-08-29 13:52:05 +0300

    Error handling added

--- currency-convert.js
+++ currency-convert.js
@@ -4,18 +4,30 @@
 // 23 USD is worth 30 CAD, You can spend it in the following countris:
 const axios = require('axios');
 
-const getExchangeRate = (from, to) => {
-  return axios.get(`http://api.fixer.io/latest?base=${from}`).then((response)=>{
-    return response.data.rates[to];
-  });
+const getExchangeRate = async (from, to) => {
+  try {
+    const response = await axios.get(`http://api.fixer.io/latest?base=${from}`)
+    const rate = response.data.rates[to];
+    if (rate){
+      return rate;
+    } else {
+      throw new Error();
+    }
+  } catch (e) {
+    throw new Error(`Unable to get exchange rate for currnecies ${from} and ${to}`);
+  }
 }
 
-const getCountries = (currenCycode) => {
-  return axios.get(`https://restcountries.eu/rest/v2/currency/${currenCycode}`).then((response) => {
+const getCountries = async (currenCycode) => {
+  try {
+    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currenCycode}`);
     return response.data.map((country)=>{
-      return country.name;
-    });
-  });
+        return country.name;
+      });
+  } catch(e){
+    throw new Error(`Unable to get countries that use ${currenCycode}`);
+  }
+
 }
 
 const convertCurrency = (from, to, amount) =>{
@@ -37,11 +49,11 @@ const convertCurrencyAsync = async (from, to, amount) => {
   return `${amount} ${from} is worth ${exchangedAmount} ${to}. ${to} can be used in the following countries ${countries.join(', ')}`;
 }
 
-convertCurrencyAsync('RUB', 'USD', 10000).then((string)=>{
+convertCurrencyAsync('USD', 'EUR', 10000).then((string)=>{
   console.log(string);
-});
-convertCurrency('USD','CAD', 250).then((string)=>{
-  console.log(string);
-});
+}).catch((e)=>{console.log(e);});
+// convertCurrency('USD','CAD', 250).then((string)=>{
+//   console.log(string);
+// });
 // getExchangeRate('USD', 'CAD').then((rate)=>{console.log(rate)});
 // getCountries('CAD').then((list)=>{console.log(list);});
